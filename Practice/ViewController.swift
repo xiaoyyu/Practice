0f55cbd9bce3897f17293861e0a5de8c35d56c00
sohekiu <sohekiu@user1nombp-2.9zh-jp.ibmmobiledemo.com>
2019-04-18 11:49:21 +0900

    Initial Commit

---
+++ Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by sohekiu on 2019/04/18.
+//  Copyright © 2019 sohekiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
